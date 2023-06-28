1a782c6ba5fca128d6bb5a771476971d59ceee24
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2023-06-28 18:04:43 +0530

    Add documentation

--- src/containers/StopWatch/StopWatch.js
+++ src/containers/StopWatch/StopWatch.js
@@ -1,28 +1,41 @@
 import { useEffect, useState } from 'react';
+
 import { Button, Number } from '../../components';
+
 import "./StopWatch.css";
 import playLogo from '../../assets/svg/play-solid.svg';
 import pauseLogo from '../../assets/svg/pause-solid.svg';
 import stopLogo from '../../assets/svg/stop-solid.svg';
 
 export function StopWatch() {
+    // Counter to count seconds in the stopwatch
     const [ seconds, setSeconds ] = useState(0);
+    // Counter to count the minutes in the stopwatch
     const [ minutes, setMinutes ] = useState(0);
+    // The separator in the middle has 2 colors. One lighter red and one darker red.
+    // These colors alternate every half second. This flag determines which color the
+    // separator will be taking.
     const [ darkSeparator, setDarkSepartor ] = useState(true);
+    // This enables or disables the counter of the stopwatch
     const [ enableStopWatch, setEnableStopWatch ] = useState(false);
 
     useEffect(() => {
         if(enableStopWatch) {
+            // Increment minutes by 1 and reset seconds to 0 if 60 seconds is reached
             if(seconds === 60) {
                 setMinutes(previousMinutes => previousMinutes + 1);
                 setSeconds(0);
             }
 
+            // Reset the stopwatch if 60 seconds is reached
             if(minutes === 60) {
                 setMinutes(0);
                 setSeconds(0);
             }
     
+            // Update the stopwatch every 500 milliseconds (half second).
+            // Every half second, the color of the separator alternates between lighter
+            // and darker red. And every second (1000 milliseconds), the seconds is incremented by 1.
             const interval = setInterval(() => {
                 if(!darkSeparator) {
                     setDarkSepartor(true);
@@ -33,6 +46,7 @@ export function StopWatch() {
                 }
             }, 500);
     
+            // When component unmounts, clear the interval.
             return () => clearInterval(interval);
         }
     }, [ darkSeparator, enableStopWatch, seconds, minutes ]);
